b6a69d1eda225c8de4b3c39320b5bc3879410b30
inblossoms <L13994352192@163.com>
2023-11-18 21:18:36 +0800

    feat: update project build configuraion

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin"),
+  { resolve } = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/run.js",
+  output: {
+    path: resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: resolve(__dirname, "./src/index.html"),
+    }),
+  ],
+  devtool: "eval-source-map",
+};
